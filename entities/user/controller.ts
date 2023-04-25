d766cc1b1e6bbd0874375e720deaae6505b4ea9d
Carlos <carlossabrido12@gmail.com>
2023-04-25 19:47:08 +0200

    function to create new users

--- entities/user/controller.ts
+++ entities/user/controller.ts
@@ -1,10 +1,17 @@
 import User from "./model.js";
 import bcrypt from 'bcrypt'
-import { error } from "console";
 import  config  from '../../core/conf.js'
 import jwt from 'jsonwebtoken'
 
 export const userList=async(req,res)=>{
    const userList1= await User.find({})
    return res.json(userList1)
+}
+
+export const createUser= async(data)=>{
+    if(!data.password || data.password.lenght < 5) throw new Error('Invalid Password')
+    data.password = await bcrypt.hash(data.password, config.SALT_ROUND)
+    data.role= 'client'
+    return User.create(data)
+
 }
--- entities/user/router.ts
+++ entities/user/router.ts
@@ -1,18 +1,19 @@
 import  express from 'express'
-import {  userList } from './controller.js'
+import {  createUser, userList } from './controller.js'
 
 const router= express.Router()
 
 router.get('/' ,userList)
 
-// router.post('/',async(req,res)=>{
-//     try{
-//         res.json(await createUser(req.body))
-//     }
-//     catch(e){
+router.post('/',async(req,res)=>{
+    try{
+        res.json(await createUser(req.body))
+    }
+    catch(e){'Problem creating user'
 
-//     }
-// })
+    }
+})
+
 
 
 
