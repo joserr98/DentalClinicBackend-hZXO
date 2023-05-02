671c0c74df5896777d48b2a95d7db328658d8959
Carlos <carlossabrido12@gmail.com>
2023-05-02 11:34:53 +0200

    create a delete fucntion + readme

--- entities/user/controller.ts
+++ entities/user/controller.ts
@@ -5,12 +5,12 @@ import jwt from 'jsonwebtoken'
 
 export const userList=async(req)=>{
     
-    const regexsurname= new RegExp(req.query.surname,'i')
+    const regexSurname= new RegExp(req.query.surname,'i')
     const regexpName= new RegExp(req.query.name,'i')
     if(req.token.role=='client'){    
-    return User.find({_id:req.token.id},{name:1,surname:1,phone_number:1,email:1})}
+    return User.find({_id:req.token.id,},{name:1,surname:1,phone_number:1,email:1,password:1,role:1})}
     else if(req.token.role=='dentist'){
-        return User.find({name:regexpName})
+        return User.find({name:regexpName,surname:regexSurname,deleted_at:null},{name:1,surname:1,phone_number:1,email:1,role:1})
     }
 }
 
@@ -18,6 +18,8 @@ export const userList=async(req)=>{
 export const updateUser = async (data) => {
     if (data.token.role === 'client' && data.params.id === data.token.id) {
       const user = await User.findOne({ _id: data.params.id });
+      if(data.body.role!=='client'){throw new Error('Clients are not allowed to change their role')}
+      data.body.password = await bcrypt.hash(data.body.password, config.SALT_ROUND)
       data.body.updated_at = new Date();
       const updateUser= await User.findOneAndUpdate({ _id: data.params.id },{$set:data.body},{ returnDocument: 'after' });
       if (!user) {
@@ -29,8 +31,10 @@ export const updateUser = async (data) => {
       if (!user) {
         throw new Error('User not found, impossible to update');
       }
+      
+      data.body.password = await bcrypt.hash(data.body.password, config.SALT_ROUND)
       data.body.updated_at = new Date();
-       const updatedUser=await User.findOneAndUpdate({ _id: data.params.id },{$set:data.body},{ returnDocument: 'after' });
+      const updatedUser=await User.findOneAndUpdate({ _id: data.params.id },{$set:data.body},{ returnDocument: 'after' });
       return updatedUser 
     } else {
       throw new Error('Invalid user role');
@@ -47,34 +51,35 @@ export const createUser= async(data)=>{
 }
 
 export const login= async(data)=>{
-    const user=await User.findOne({email:data.email});
-    if(!user) return null;
+    const user=await User.findOne({email:data.email,deleted_at:null});
+    if(!user) return ('user not found');
     if(!(await bcrypt.compare(data.password,user.password))) return null;
     const token= jwt.sign({id: user._id, role: user.role}, config.SECRET, {expiresIn: '24h'})
     return{token}
 }
 
 export const deleteUser= async (data) => {
-    console.log('hola')
-    if(data.token.role=='client' && data.params.id === data.token.id){
-    const deleteClient = await User.findOne({ _id: data.params.id });
-    if (!deleteClient) throw new Error("User not found");
+    
+  if (data.token.role === 'client' && data.params.id === data.token.id) {
+    const user = await User.findOne({ _id: data.params.id });
     data.body.deleted_at = new Date();
-    await User.findOneAndReplace({ _id: data.params.id },{$set:data.body},{ returnDocument: 'after' });
-    return await deleteClient.save()
+    const updateUser= await User.findByIdAndUpdate({ _id: data.params.id },{$set:data.body},{ returnDocument: 'after' });
+    if (!user) {
+      throw new Error('User not found, impossible to update');
     }
+    return updateUser;
+  } else if (data.token.role === 'dentist') {
+    const user = await User.findById(data.params.id);
+    if (!user) {
+      throw new Error('User not found, impossible to update');
+    }
+    data.body.deleted_at = new Date();
+     const updatedUser=await User.findByIdAndUpdate({ _id: data.params.id },{$set:data.body},{ returnDocument: 'after' });
+    return updatedUser 
+  } else {
+    throw new Error('Invalid user role');
+  }
 };
-
-    // else if(data.token.role=='dentist'){
-    //  const user= await User.findById(data.params.id)
-    //  if(!user){
-    //     throw new Error("User not Found")
-    //  }
-    //  data.body.deleted_at = new Date();
-    //  const deleteUser= await User.findOneAndReplace({ _id: data.params.id },{$set:data.body},{ returnDocument: 'after' });
-    //  return deleteUser
-    // }
-    // else{throw new Error('Invalid user role');}
 
 
  
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Dental Clinic
+
+Project focused in create a Denatal Clinic data base and make it works properly
+
+# Functionality
+
+First of all  we have 2 categories "clients" and "Dentist"
+
+The point is make it works as a professional and eficient data base, so for show it better we are gonna introduce you the mains properties of each group
+
+# Clients
+
+- Clients need to be able to create new account in the server
+- Clientes must be able to log in
+- Clientes should have access to their personal information
+- Clients only can modify their personal information and not the imformation from other people
+- If the cliente decide delete his account he will need to create a new acount to become a client again
+
+# Appointment
+
+
+# Technologies used
+
+
+<img src="media/js.png" alt="JavaScript" width="50">
+<img src="media/descarga.png" alt="JavaScript" width="50">
+<img src="media/nodo-js.png" alt="JavaScript" width="50">
+
+
+# Inquires 
+
+Proyect developed in a team if you got any inquires get in touch by Linkedn with any of us
+https://www.linkedin.com/in/carlos-sabrido-medina-624b77258/
